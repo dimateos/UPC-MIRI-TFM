68784b63282b9414b8b57eb2ab14975fab3f29b9
Diego Mateos <dimateos@ucm.es>
2023-03-02 05:58:41 +0100

    voro++ interface fails to install for python310?

--- scripts/env-test.bat
+++ scripts/env-test.bat
@@ -12,3 +12,13 @@ bpy -c "help(\"modules\")"
 
 REM autocomplete for bpy and bmesh etc
 bpip install fake-bpy-module-3.4
+
+REM voro++ interface for this python version...
+bpip install wheel
+bpip install setuptools-scm
+bpip install setuptools
+
+REM all fail when compiling c++ interface... 'Python.h': No such file or directory
+bpip install tess
+bpip install git+https://github.com/eleftherioszisis/tess
+bpip install git+https://github.com/joe-jordan/pyvoro@feature/python3
