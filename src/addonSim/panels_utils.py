fa8f3cf2bd525c4f37215ccd93b1cbb2f918aaff
Diego Mateos <dimateos@ucm.es>
2023-04-24 20:00:23 +0200

    split panel utils

--- src/addonSim/panels_utils.py
+++ src/addonSim/panels_utils.py
@@ -2,15 +2,7 @@ import bpy
 import bpy.types as types
 import bpy.props as props
 
-from .preferences import getPrefs
-from .properties import (
-    MW_gen_cfg,
-    MW_sim_cfg,
-    MW_vis_cfg,
-)
-from . import operators as ops
 from . import operators_utils as ops_util
-from .panels_utils import util_classes_pt
 
 from . import ui
 from . import utils
@@ -21,85 +13,7 @@ PANEL_CATEGORY = "Dev"
 PANEL_INFO_NOTIFY_NO_SELECTED = False
 
 
-# OPT:: split panel utils from main
 #-------------------------------------------------------------------
-
-class MW_gen_PT(types.Panel):
-    bl_category = PANEL_CATEGORY
-    bl_label = "MW_gen"
-    bl_idname = "MW_PT_gen"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    bl_context = "objectmode"
-    bl_options = {'HEADER_LAYOUT_EXPAND'}
-
-    def draw(self, context):
-        prefs = getPrefs()
-        layout = self.layout
-        col = layout.column()
-
-        # Something selected, not last active
-        if not context.selected_objects:
-            col.label(text="No object selected...", icon="ERROR")
-            return
-
-        if not context.active_object:
-            col.label(text="Selected but removed active?", icon="ERROR")
-            return
-
-        obj, cfg = utils.cfg_getRoot(context.active_object)
-
-        # No fracture selected
-        if not cfg:
-            col.label(text="Selected: " + obj.name_full, icon="INFO")
-
-            # Check that it is a mesh
-            if obj.type != 'MESH':
-                col = layout.column()
-                col.label(text="Select a mesh...", icon="ERROR")
-                return
-
-            # Fracture original object
-            col = layout.column()
-            col.operator(ops.MW_gen_OT_.bl_idname, text="GEN Fracture", icon="STICKY_UVS_DISABLE")
-
-        # Edit/info of selected
-        else:
-            col = layout.column()
-            col.label(text="Root: " + obj.name_full, icon="INFO")
-
-            col.operator(ops.MW_gen_OT_.bl_idname, text="EDIT Fracture", icon="STICKY_UVS_VERT")
-
-            col_rowSplit = col.row().split(factor=0.66)
-            col_rowSplit.operator(ops.MW_util_delete_OT_.bl_idname, text="DELETE rec", icon="CANCEL")
-            prefs = getPrefs()
-            col_rowSplit.prop(prefs, "OT_util_delete_unhide")
-
-            ui.draw_propsToggle(cfg, prefs, "PT_gen_show_summary", "PT_gen_propFilter", "PT_gen_propEdit", col)
-
-
-class MW_addon_PT(types.Panel):
-    bl_category = PANEL_CATEGORY
-    bl_label = "MW_addon"
-    bl_idname = "MW_PT_addon"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-    #bl_context = "objectmode"
-    bl_options = {'DEFAULT_CLOSED'}
-
-    def draw(self, context):
-        prefs = getPrefs()
-        layout = self.layout
-        col = layout.column()
-
-        ui.draw_propsToggle(prefs, prefs, "meta_show_prefs", "meta_propFilter", "meta_propEdit", col)
-
-        open, box = ui.draw_toggleBox(prefs, "meta_show_tmpDebug", layout)
-        if open:
-            col = box.column()
-            # check region width
-            DEV.draw_val(col, "context.region.width", context.region.width)
-
 
 class Info_Inpect_PT(types.Panel):
     bl_category = PANEL_CATEGORY
@@ -109,6 +23,20 @@ class Info_Inpect_PT(types.Panel):
     bl_region_type = "UI"
     #bl_context = "objectmode"
     bl_options = {'DEFAULT_CLOSED'}
+
+    edit_showWorld: props.BoolProperty(
+        name="world space", description="Show vertices positions in world space",
+        default=False,
+    )
+    edit_showEdges: props.BoolProperty(
+        name="Show edges...", description="Show long list of edges with its key (v1, v2)",
+        default=False,
+    )
+    edit_showFaceCenters: props.BoolProperty(
+        name="center position", description="Show face center position instead of vertex indices",
+        default=False,
+    )
+
 
     def draw(self, context):
         if bpy.context.mode == 'OBJECT':
@@ -149,7 +77,6 @@ class Info_Inpect_PT(types.Panel):
 
     def draw_editMode(self, context):
         layout = self.layout
-        prefs = getPrefs()
         obj = bpy.context.object
 
         mainCol, mainBox = ui.draw_inspectObject(obj, layout, drawTrans=False)
@@ -178,14 +105,14 @@ class Info_Inpect_PT(types.Panel):
         row = col.row()
         row.alignment= "LEFT"
         row.label(text=f"verts: {len(selected_verts)}")
-        row.prop(prefs, "PT_info_edit_showWorld")
+        row.prop(self, "edit_showWorld")
         for v in selected_verts:
-            if prefs.PT_info_edit_showWorld: pos = obj.matrix_world @ v.co
+            if self.edit_showWorld: pos = obj.matrix_world @ v.co
             else: pos = v.co
             col.label(text=f"{v.index}: " + f"{fmt_vec}".format(*pos))
 
         # optional edges
-        open, box = ui.draw_toggleBox(prefs, "PT_info_edit_showEdges", mainCol)
+        open, box = ui.draw_toggleBox(self, "edit_showEdges", mainCol)
         if open:
             col = box.column()
             col.label(text=f"edges: {len(selected_edges)}")
@@ -197,10 +124,10 @@ class Info_Inpect_PT(types.Panel):
         row = col.row()
         row.alignment= "LEFT"
         row.label(text=f"faces: {len(selected_faces)}")
-        row.prop(prefs, "PT_info_edit_showFaceCenters")
-        if (prefs.PT_info_edit_showFaceCenters):
+        row.prop(self, "edit_showFaceCenters")
+        if (self.edit_showFaceCenters):
             for f in selected_faces:
-                if prefs.PT_info_edit_showWorld: pos = obj.matrix_world @ f.center
+                if self.edit_showWorld: pos = obj.matrix_world @ f.center
                 else: pos = f.center
                 col.label(text=f"{f.index}: " + f"{fmt_vec}".format(*pos))
         else:
@@ -209,19 +136,6 @@ class Info_Inpect_PT(types.Panel):
 #-------------------------------------------------------------------
 # Blender events
 
-# sort to set default order?
-classes = [
-    MW_gen_PT,
+util_classes_pt = [
+    Info_Inpect_PT,
 ]
-+ util_classes_pt
-+ [
-    MW_addon_PT,
-]
-
-def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
-
-def unregister():
-    for cls in reversed(classes):
-        bpy.utils.unregister_class(cls)
