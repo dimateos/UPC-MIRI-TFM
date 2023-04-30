a169793fbda1f9f50f34c6c1c7399daa975d3675
Diego Mateos <dimateos@ucm.es>
2023-04-30 11:21:23 +0200

    started to remove edit fracture suport

--- src/addonSim/operators.py
+++ src/addonSim/operators.py
@@ -39,6 +39,8 @@ class MW_gen_OT(_StartRefresh_OT):
         self.refresh_log = True
         self.end_log = True
 
+    # NOTE:: no poll because the button is removed from ui isntead
+
     def draw(self, context: types.Context):
         super().draw(context)
         ui.draw_gen_cfg(self.cfg, self.layout, context)
@@ -49,9 +51,6 @@ class MW_gen_OT(_StartRefresh_OT):
         return super().invoke(context, event)
 
     #-------------------------------------------------------------------
-
-    # IDEA:: changelog / todo file to take notes there instead of all in the code?
-    # NOTE:: no poll because the button is removed from ui isntead
 
     # OPT:: GEN: more error handling of user deletion of intermediate objects?
     # OPT:: automatically add particles -> most interesting method...
@@ -80,54 +79,70 @@ class MW_gen_OT(_StartRefresh_OT):
         # IDEA:: decimate before/after convex, test perf?
 
 
-        # Need to copy the properties from the object if its already a fracture
+        # Retrieve root
         obj, cfg = utils.cfg_getRoot(context.active_object)
         getStats().logDt("retrieved root object")
 
-        # Selected object not fractured
+        # Selected object not fractured, fresh execution
         if not cfg:
             DEV.log_msg("cfg NOT found: new frac", {'SETUP'})
-            cfg: MW_gen_cfg = self.cfg
+            obj_root, obj_original = mw_setup.gen_copyOriginal(obj, self.cfg, context)
+            return self.execute_fresh(context, obj_root, obj_original)
 
-            # TODO:: convex hull triangulates the faces... just decimate pls
-            obj, obj_original = mw_setup.gen_copyOriginal(obj, cfg, context)
-            if cfg.shape_useConvexHull:
-                obj_toFrac = mw_setup.gen_copyConvex(obj, obj_original, cfg, context)
-            else: obj_toFrac = obj_original
-
-        # Copy the config to the operator once
+        # fracture the same original object for an alternative result
         else:
-            if "NONE" in self.cfg.meta_type:
-                DEV.log_msg("cfg found: copying props to OP", {'SETUP'})
-                copyProps(cfg, self.cfg)
-                return self.end_op("PASS_THROUGH init copy of props")
+            DEV.log_msg("cfg found: duplicating frac", {'SETUP'})
+            copyProps(cfg, self.cfg)
+            obj_original = utils.get_child(obj, mw_setup.CONST_NAMES.original_copy)
+            obj_copu = utils.copy_objectRec(obj_original, context)
+            obj_original.name = cfg.struct_nameOriginal
+            return self.execute_fresh(context, obj_root, obj_original)
 
-            else:
-                DEV.log_msg("cfg found: getting toFrac child", {'SETUP'})
-                cfg: MW_gen_cfg = self.cfg
+        # NOTE:: no longer supporting edit fracture -> basically always replacing geometry hence being slower
+        ## Config found in the object
+        #else:
+        #    # First execute just copy the cfg
+        #    if "NONE" in self.cfg.meta_type:
+        #        DEV.log_msg("cfg found: copying props to OP", {'SETUP'})
+        #        copyProps(cfg, self.cfg)
+        #        return self.end_op("PASS_THROUGH init copy of props")
 
-                name_original = f"{mw_setup.CONST_NAMES.original}{cfg.struct_nameOriginal}"
-                obj_original = utils.get_child(obj, name_original)
-                if cfg.shape_useConvexHull:
-                    obj_toFrac = utils.get_child(obj, mw_setup.CONST_NAMES.original_c)
-                else: obj_toFrac = obj_original
-                getStats().logDt("retrieved toFrac object")
+        #    # Later runs edit optimized
+        #    else:
+        #        return self.execute_edit(context, obj)
 
-
-        # IDEA:: global rnd in prefs?
-        cfg.rnd_seed = utils.rnd_seed(cfg.rnd_seed) # seed common random gen
+    def execute_fresh(self, context: types.Context, obj_root:types.Object, obj_original:types.Object ):
+        cfg: MW_gen_cfg = self.cfg
         prefs = getPrefs()
+        cfg.rnd_seed = utils.rnd_seed(cfg.rnd_seed)
 
-        # XXX:: generation from zero is more done twice faster than generating -> deleting all objects previosly?
 
-        DEV.log_msg("Start calc points", {'SETUP'})
+
+        DEV.log_msg("Initial object setup", {'SETUP'})
+        # TODO:: convex hull triangulates the faces... e.g. UV sphere ends with more!
+        if cfg.shape_useConvexHull:
+            obj_toFrac = mw_setup.gen_copyConvex(obj_root, obj_original, cfg, context)
+        else: obj_toFrac = obj_original
+
+        # Rename and select the root
+        mw_setup.gen_renaming(obj_root, cfg, context)
+        utils.select_unhide(obj_root, context)
+
+        # Add edited cfg to the object
+        copyProps(self.cfg, obj_root.mw_gen)
+        getStats().logDt("copied prop to root object")
+        return self.end_op()
+
+
+
+        DEV.log_msg("Start calc points", {'CALC'})
         # Get the points and transform to local space when needed
         mw_calc.detect_points_from_object(obj_original, cfg, context)
         points = mw_calc.get_points_from_object_fallback(obj_original, cfg, context)
         if not points:
             return self.end_op_error("found no points...")
 
-        # Get more data
+        # Get more data from the points
         bb, bb_radius = utils.get_bb_radius(obj_toFrac, cfg.margin_box_bounds)
         if cfg.shape_useWalls:
             faces4D = utils.get_faces_4D(obj_toFrac, cfg.margin_face_bounds)
@@ -136,13 +151,17 @@ class MW_gen_OT(_StartRefresh_OT):
         # XXX:: limit particles axis too?
         # XXX:: child verts / partilces should be checked inside?
 
-
-        # Limit and rnd a bit the points and add them to the scene
+        # Limit and rnd a bit the points
         mw_calc.points_transformCfg(points, cfg, bb_radius)
 
+        # Add some reference of the points to the scene
+        mw_setup.gen_pointsObject(obj, points, self.cfg, context)
+        mw_setup.gen_boundsObject(obj, bb, self.cfg, context)
+        getStats().logDt("spawned points objs")
 
-        # Calc voronoi
-        DEV.log_msg("Start calc cont", {'SETUP'})
+
+
+        DEV.log_msg("Start calc cont and links", {'CALC'})
 
         # IDEA:: mesh conecting input points + use single mesh instead of one per link?
         # XXX:: detect meshes with no volume? test basic shape for crashes...
@@ -151,59 +170,58 @@ class MW_gen_OT(_StartRefresh_OT):
 
         cont = mw_calc.cont_fromPoints(points, bb, faces4D, precision=prefs.calc_precisionWalls)
         if not cont:
-            self.setup_final(obj, context, points, bb)
             return self.end_op_error("found no cont... but could try recalculate!")
 
-        cfg.nbl_cont = cont
+        #cfg.nbl_cont = cont
         obj_shards = mw_setup.gen_shardsEmpty(obj, cfg, context)
 
-        #test some legacy or statistics stuff
+        #test some legacy or statistics cont stuff
         if DEV.LEGACY_CONT:
             mw_setup._gen_LEGACY_CONT(obj_shards, cont, cfg, context)
-            self.setup_final(obj, context, points, bb)
             return self.end_op("DEV.LEGACY_CONT stop...")
         mw_setup.gen_shardsObjects(obj_shards, cont, cfg, context, invertOrientation=prefs.gen_invert_shardNormals)
 
-
-        DEV.log_msg("Start calc links", {'SETUP'})
         links = Links(cont, obj_shards)
 
 
-        # WIP:: links better generated from map isntead of cont?
-        obj_links, obj_links_toWall, obj_links_perCell = mw_setup.gen_linksEmpties(obj, cfg, context)
-        if links.link_map:
-            mw_setup.gen_linksObjects(obj_links, obj_links_toWall, links, cfg, context)
-        else:
-            utils.delete_objectRec(obj_links)
-            utils.delete_objectRec(obj_links_toWall)
-        mw_setup.gen_linksCellObjects(obj_links_perCell, cont, cfg, context)
+        ## WIP:: links better generated from map isntead of cont? + done in a separate op
+        #obj_links, obj_links_toWall, obj_links_perCell = mw_setup.gen_linksEmpties(obj, cfg, context)
+        #if links.link_map:
+        #    mw_setup.gen_linksObjects(obj_links, obj_links_toWall, links, cfg, context)
+        #else:
+        #    utils.delete_objectRec(obj_links)
+        #    utils.delete_objectRec(obj_links_toWall)
+        #mw_setup.gen_linksCellObjects(obj_links_perCell, cont, cfg, context)
 
-
-        self.setup_final(obj, context, points, bb)
         return self.end_op()
-
-    # OPT:: maybe use class atttrs instead of args
-    def setup_final(self, obj: types.Object, ctx: types.Context, points=None, bb=None):
-        DEV.log_msg("Do final scene setup", {'SETUP'})
-
-        # Select after renaming
-        mw_setup.gen_renaming(obj, self.cfg, ctx)
-        utils.select_unhide(obj, ctx)
-        getStats().logDt("renamed and selected")
-
-        # OPT:: prefs to select what to add
-        # spawn more visible objects
-        if points and bb:
-            mw_setup.gen_pointsObject(obj, points, self.cfg, ctx)
-            mw_setup.gen_boundsObject(obj, bb, self.cfg, ctx)
-            getStats().logDt("spawned points objs")
-
-        # Add edited cfg to the object
-        copyProps(self.cfg, obj.mw_gen)
 
 #-------------------------------------------------------------------
 
-# OPT:: maybe delete hierarchy fail due to some issue caused by me
+class MW_gen_links_OT(_StartRefresh_OT):
+    bl_idname = "mw.gen_links"
+    bl_label = "Generate links object"
+    bl_description = "Generate a visual representation of the links of a fracture object"
+
+    # UNDO as part of bl_options will cancel any edit last operation pop up
+    bl_options = {'INTERNAL', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        # poll execute on ui draw, so only check if has root, dont extract it
+        obj = context.active_object
+        return (obj and utils.cfg_hasRoot(obj))
+
+    def execute(self, context: types.Context):
+        self.start_op()
+        obj, cfg = utils.cfg_getRoot(context.active_object)
+        prefs = getPrefs()
+
+        #todo
+
+        return self.end_op()
+
+#-------------------------------------------------------------------
+
 class MW_util_delete_OT(_StartRefresh_OT):
     bl_idname = "mw.util_delete"
     bl_label = "Delete fracture object"
@@ -211,23 +229,16 @@ class MW_util_delete_OT(_StartRefresh_OT):
 
     # UNDO as part of bl_options will cancel any edit last operation pop up
     bl_options = {'INTERNAL', 'UNDO'}
-    _obj: types.Object = None
-    _cfg: MW_gen_cfg = None
-
-    def __init__(self) -> None:
-        super().__init__()
-        # config some base class log flags...
-        self.start_resetStats = False
 
     @classmethod
     def poll(cls, context):
-        obj, cfg = utils.cfg_getRoot(context.active_object)
-        MW_util_delete_OT._obj, MW_util_delete_OT._cfg = obj, cfg
-        return (obj and cfg)
+        # poll execute on ui draw, so only check if has root, dont extract it
+        obj = context.active_object
+        return (obj and utils.cfg_hasRoot(obj))
 
     def execute(self, context: types.Context):
         self.start_op()
-        obj, cfg = MW_util_delete_OT._obj, MW_util_delete_OT._cfg
+        obj, cfg = utils.cfg_getRoot(context.active_object)
         prefs = getPrefs()
 
         # optional unhide flag
